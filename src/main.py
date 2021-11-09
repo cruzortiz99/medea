1df12c6fdbf46b342efd542d92a2036326bd9409
Cruz <cruzortiz099@gmail.com>
2021-11-09 07:40:43 -0400

    uploading to work with less threads

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 
 import rx
 from flask import Flask
-from rx.operators import subscribe_on
 
 from config import HOST, PORT
 from utils.browser import open_web_browser
-from utils.threadpoll import POOL_SCHEDULER
 
 APP = Flask(__name__)
 
@@ -15,19 +13,12 @@ def home():
     return rx.of("Hola mundo").run()
 
 
-def run_server(observer: rx.core.observer.observer.Observer, _):
+def run_server():
     APP.run(host=HOST, port=PORT)
-    observer.on_next(True)
-    observer.on_completed()
 
 
 if __name__ == '__main__':
-    rx.merge(
-        rx.create(open_web_browser(HOST, PORT)).pipe(
-            subscribe_on(POOL_SCHEDULER)),
-        rx.create(run_server)
-    ).subscribe(
-        on_next=print,
+    rx.create(open_web_browser(HOST, PORT)).subscribe(
+        on_next=lambda _: run_server(),
         on_error=print,
-        on_completed=lambda: print("Done")
-    )
+        on_completed=print)
--- src/utils/threadpoll/__init__.py
+++ src/utils/threadpoll/__init__.py
@@ -1,6 +1,8 @@
 
 
+from threading import main_thread
 from rx.scheduler.threadpoolscheduler import ThreadPoolScheduler
 import multiprocessing
 
 POOL_SCHEDULER = ThreadPoolScheduler(multiprocessing.cpu_count())
+MAIN_THREAD = main_thread()
