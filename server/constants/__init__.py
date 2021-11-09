48a15ff413c9a473e28fd6444b3ff2cf244570ef
Cruz <cruzortiz099@gmail.com>
2021-11-09 12:44:23 -0400

    putting in prod

--- server/constants/__init__.py
+++ server/constants/__init__.py
@@ -1,3 +1,3 @@
 from pathlib import Path
 
-ASSETS_FOLDER = Path(__file__).parent.parent.parent.joinpath("assets")
+ASSETS_FOLDER = Path(__file__).parent.parent.joinpath("assets")
--- server/main.py
+++ server/main.py
@@ -4,9 +4,11 @@ from flask import Flask, send_file
 
 from config import HOST, PORT
 from utils.browser import open_web_browser
-from utils.constants import ASSETS_FOLDER
+from constants import ASSETS_FOLDER
 
-APP = Flask(__name__, static_folder=ASSETS_FOLDER, static_url_path="/static")
+APP = Flask(
+    __name__,
+    static_folder=ASSETS_FOLDER, static_url_path="/")
 
 
 @APP.route("/", methods=["GET"])
