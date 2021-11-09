1f7211ea7d38f05b6f7f1b6b0a4052ff3b405d50
Cruz <cruzortiz099@gmail.com>
2021-11-09 08:01:08 -0400

    adding static files

--- src/main.py
+++ src/main.py
@@ -1,24 +1,21 @@
 
 import rx
-from flask import Flask
+from flask import Flask, send_file
 
 from config import HOST, PORT
 from utils.browser import open_web_browser
+from utils.constants import ASSETS_FOLDER
 
-APP = Flask(__name__)
+APP = Flask(__name__, static_folder=ASSETS_FOLDER, static_url_path="/static")
 
 
 @APP.route("/", methods=["GET"])
 def home():
-    return rx.of("Hola mundo").run()
-
-
-def run_server():
-    APP.run(host=HOST, port=PORT)
+    return send_file(ASSETS_FOLDER.joinpath("index.html"))
 
 
 if __name__ == '__main__':
     rx.create(open_web_browser(HOST, PORT)).subscribe(
-        on_next=lambda _: run_server(),
+        on_next=lambda _: APP.run(host=HOST, port=PORT),
         on_error=print,
         on_completed=print)
---
+++ src/utils/constants/__init__.py
@@ -0,0 +1,3 @@
+from pathlib import Path
+
+ASSETS_FOLDER = Path(__file__).parent.parent.parent.joinpath("assets")
