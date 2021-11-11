03e4d071e028056c93005757345f0d9b5e1b1ff6
Cruz <cruzortiz099@gmail.com>
2021-11-11 10:32:27 -0400

    handling sidebar

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from "react"
-import { Container, Content, Header, Sidenav, Sidebar, Button } from "rsuite"
+import React, { useState } from "react"
+import { Button, Container, Content, Nav, Sidebar, Sidenav } from "rsuite"
+import { Dashboard } from "@rsuite/icons"
 import { AppRoutedPage } from "../../routes/routes"
 import { joinClasses } from "../../utils/css-classes"
 import styles from "./Home.module.css"
@@ -8,22 +9,40 @@ function App(props: AppRoutedPage) {
   const [sideBarOpen, setSideBarOpen] = useState(true)
   return (
     <Container className={joinClasses(styles.appPage)}>
-      <Sidebar collapsible width={sideBarOpen ? 270 : 70}>
+      <Sidebar collapsible width={sideBarOpen ? 270 : 76}>
         <Sidenav
           className={joinClasses(
             styles.sidebar,
             sideBarOpen ? styles.open : styles.close
           )}
-          expanded={sideBarOpen}
         >
-          <Sidenav.Header
-            className={joinClasses(styles.sidebar, styles.header)}
-          >
-            <h3>MENEA</h3>
+          <Sidenav.Header>
+            <Nav className={joinClasses(styles.sidebar, styles.header)}>
+              <Nav.Item
+                icon={
+                  <Dashboard
+                    style={{
+                      fontSize: "25px",
+                      height: "25px",
+                    }}
+                  />
+                }
+                style={{ paddingTop: "0.25rem", height: "100%" }}
+              >
+                {sideBarOpen && <h3>MENEA</h3>}
+              </Nav.Item>
+            </Nav>
           </Sidenav.Header>
           <Sidenav>
-            <Sidenav.Body className={joinClasses(styles.sidebar, styles.body)}>
-              Links
+            <Sidenav.Body>
+              <Nav className={joinClasses(styles.sidebar, styles.body)}>
+                <Nav.Item icon={<Dashboard />}>
+                  {sideBarOpen && "Avisos y Fallas"}
+                </Nav.Item>
+                <Nav.Item icon={<Dashboard />}>
+                  {sideBarOpen && "Avisos"}
+                </Nav.Item>
+              </Nav>
             </Sidenav.Body>
           </Sidenav>
           <Container className={joinClasses(styles.sidebar, styles.footer)}>
