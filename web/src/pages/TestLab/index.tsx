4f1867340dc2b7f65d55f1db7f676d523954a894
Cruz <cruzortiz099@gmail.com>
2022-02-18 08:04:15 -0400

    updating to add an example of fetch data

--- web/src/pages/TestLab/index.tsx
+++ web/src/pages/TestLab/index.tsx
@@ -1,49 +1,48 @@
 import React, { useEffect, useState } from "react"
-import { Container, Table } from "rsuite"
-import { async } from "rxjs"
-import { APINoteM3 } from "../../models"
-
+import { Table } from "rsuite"
+import { map, tap } from "rxjs"
+import { fromFetch } from "rxjs/fetch"
+import { APINoteM3, APIResponse } from "../../models"
 
 function TestPage() {
   const [test, setTest] = useState<APINoteM3[]>([])
-  const dataTable =  test.map((item) => (
+  useEffect(() => {
+    const getDataPipe = fromFetch<APIResponse<APINoteM3[]>>(
+      "http://localhost:5000/api/alerts-and-failures/note-m3",
       {
-        executor: item.executor,
-        amount: item.amount,
-        hours: item.hours,
-        withOutFF: item.withOutFF
+        mode: "cors",
+        method: "GET",
+        selector: (response) => response.json(),
       }
     )
-  )
-  
-  const dataTest: APINoteM3[] = dataTable
-  useEffect(() => {
-    fetch("http://localhost:5000/api/alerts-and-failures/note-m3", {mode: "cors", method: "GET"}).then(async (response) => {
-      setTest( await response.json())
-      console.log(response)
-    })
+      .pipe(tap((responseJson) => console.log(responseJson)))
+      .subscribe({
+        next: (responseJson) => setTest(responseJson.data),
+        error: () => setTest([]),
+      })
+    return () => {
+      getDataPipe.unsubscribe()
+    }
   }, [])
-  return(
+  return (
     <>
       <h2>TestLab</h2>
-      <Table
-        data={dataTest}
-      >
+      <Table data={test}>
         <Table.Column>
           <Table.HeaderCell>Amount</Table.HeaderCell>
-          <Table.Cell dataKey="amount"/>
+          <Table.Cell dataKey="amount" />
         </Table.Column>
         <Table.Column>
           <Table.HeaderCell>Executor</Table.HeaderCell>
-          <Table.Cell dataKey="excecutor"/>
+          <Table.Cell dataKey="excecutor" />
         </Table.Column>
         <Table.Column>
           <Table.HeaderCell>Hours</Table.HeaderCell>
-          <Table.Cell dataKey="hours"/>
+          <Table.Cell dataKey="hours" />
         </Table.Column>
         <Table.Column>
           <Table.HeaderCell>Wiht out FF</Table.HeaderCell>
-          <Table.Cell dataKey="withOutFF"/>
+          <Table.Cell dataKey="with_out_ff" />
         </Table.Column>
       </Table>
       <>{console.log(test)}</>
