106825ecc01dff41feca4c06ab39bae36d5b23cd
Cruz <cruzortiz099@gmail.com>
2022-02-23 15:48:27 -0400

    updating tests

--- server/services/test_note_m.py
+++ server/services/test_note_m.py
@@ -4,7 +4,7 @@ import unittest
 import rx
 from rx import operators as op
 
-from modules.alerts_and_failures import get_notes_m3
+from services.note_m import get_notes_m3
 from models.NoteM import NoteM
 
 
