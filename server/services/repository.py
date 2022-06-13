441f0432eec0b2e2f08d41493f544a5b2756acbc
Cruz <cruzortiz099@gmail.com>
2022-06-13 10:07:01 -0400

    changing match statement to if

--- server/services/repository.py
+++ server/services/repository.py
@@ -5,24 +5,24 @@ from constants import ASSETS_FOLDER
 import pandas as pd
 from pandas.core.frame import DataFrame
 
+
 def getFolders(path: str):
     directory = pathlib.Path(str(ASSETS_FOLDER) + '/storage/' + path)
     if directory.exists():
         if directory.is_file():
             return getFile(directory.suffix, directory)
-        
+
         folders = []
         for folder in directory.iterdir():
             folders.append(Repository(folder.name))
         return folders
     return []
 
+
 def getFile(suffix: str, directory: str):
-    match suffix:
-        case '.csv':
-            return readCsv(directory)
+    if suffix == ".csv":
+        return readCsv(directory)
+
 
 def readCsv(directory):
     return pd.read_csv(directory, sep=";")
-
- 
